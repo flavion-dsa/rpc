05c0099bb7ff06ad04864eb1ac63e36791ca35c9
Flavion <flaviondsa@yahoo.in>
2018-12-01 23:09:45 +0530

    Added robots.txt

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -3,7 +3,7 @@ function fetchTodaysData(callback) {
 
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today', true);
+  xobj.open('GET', '//calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today', true);
   xobj.onreadystatechange = function() {
     if (xobj.readyState == 4 && xobj.status == "200") {
       callback(xobj.responseText);
